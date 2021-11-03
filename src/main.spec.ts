34a475b7288c1381d88602120bdcd2ee382556c2
Alberto Basalo <albertobasalo@hotmail.com>
2021-11-03 18:00:00 +0100

    feat: initial implementation

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,14 +1,25 @@
-import { sayHello, sum } from './main';
+import { createAgent, hashOf } from './main';
 
-test('sayHello function exists', () => {
-  expect(sayHello).toBeDefined();
+test('Good agents are happy', () => {
+  const alice = createAgent();
+  const bob = createAgent();
+  alice.addAgent(bob);
+  bob.addAgent(alice);
+  alice.addData("Hello Bob! -Alice");
+  bob.addData("Hello Alice! -Bob");
 });
 
-describe('The sum function', () => {
-  const sut = sum;
-  it('should sum numbers', () => {
-    const actual = sut(1, 2);
-    const expected = 3;
-    expect(actual).toBe(expected);
-  });
+test('Bad agents were caught', () => {
+  const alice = createAgent();
+  const data = "bad things";
+  const prev = hashOf("");
+  expect(() => {
+    alice.receiveBlock({
+      data,
+      prev,
+      token: "",
+      hash: hashOf(data + prev),
+    });
+  }).toThrow();
+
 });
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,61 @@
-export { sayHello, sum };
+import { createHash } from "crypto";
 
-function sayHello(yourName: string): void {
-  console.log(`Hello ${yourName}`);
+export function hashOf(str: string): string {
+  return createHash("sha256").update(str).digest("hex");
 }
-function sum( a: number, b: number ): number {
-  if ( a == 0 ) {
-    return b;
-  }
-  return a + b;
-}
-const myName = 'Quark';
-sayHello(myName);
+
+type Block = {
+  data: string;
+  prev: string;
+  token: string;
+  hash: string;
+};
+type Agent = {
+  addAgent(agent: Agent): void;
+  receiveBlock(block: Block): void;
+  addData(data: Block["data"]): void;
+};
+const magicPrefix = Array(5).fill("0").join("");
+
+export const createAgent = (): Agent => {
+  const chain: Block[] = [{
+    data: "",
+    prev: "",
+    token: "",
+    hash: hashOf(""),
+  }];
+  const agents: Agent[] = [];
+  return {
+    addAgent(agent) { agents.push(agent); },
+    addData(data) {
+      while (true) {
+        const prev = chain[chain.length - 1].hash;
+        const token = hashOf(Math.random().toString());
+        const hash = hashOf(data + prev + token);
+        if (hash.startsWith(magicPrefix)) {
+          const block: Block = { data, prev, token, hash };
+          chain.push(block);
+          for (const agent of agents) {
+            agent.receiveBlock(block);
+          }
+          return;
+        }
+      }
+    },
+    receiveBlock(block) {
+      if (block.prev != chain[chain.length - 1].hash) {
+        throw new Error(
+          "Hash does not point to the previous hash in the chain",
+        );
+      }
+      if (!block.hash.startsWith(magicPrefix)) {
+        throw new Error("Hash does not start with the magic prefix");
+      }
+      const actualHash = hashOf(block.data + block.prev + block.token);
+      if (actualHash !== block.hash) {
+        throw new Error("Hash is not the hash of data|prev|token");
+      }
+      chain.push(block);
+    },
+  };
+};
